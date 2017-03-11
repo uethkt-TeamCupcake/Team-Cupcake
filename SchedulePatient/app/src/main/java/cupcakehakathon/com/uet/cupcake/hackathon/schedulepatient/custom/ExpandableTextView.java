90f4c40652b624ee41fc319dc0173888dc238a9c
luongtran <luongtv96@gmail.com>
2017-03-12 00:06:06 +0700

    Update hospital detail

---
+++ SchedulePatient/app/src/main/java/cupcakehakathon/com/uet/cupcake/hackathon/schedulepatient/custom/ExpandableTextView.java
@@ -0,0 +1,87 @@
+package cupcakehakathon.com.uet.cupcake.hackathon.schedulepatient.custom;/*
+ * Copyright (C) 2011 The Android Open Source Project
+ * Copyright 2014 Manabu Shimobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+import cupcakehakathon.com.uet.cupcake.hackathon.schedulepatient.R;
+
+public class ExpandableTextView
+    extends TextView
+    implements View.OnClickListener {
+    private static final int MAX_LINES = 4;
+    private int currentMaxLines = Integer.MAX_VALUE;
+
+    public ExpandableTextView(Context context) {
+        super(context);
+        setOnClickListener(this);
+    }
+
+    public ExpandableTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setOnClickListener(this);
+    }
+
+    public ExpandableTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOnClickListener(this);
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        /* If text longer than MAX_LINES set DrawableBottom - I'm using '...' icon */
+        post(new Runnable() {
+            public void run() {
+                if (getLineCount() > MAX_LINES) {
+                    setCompoundDrawablesWithIntrinsicBounds(0,
+                                                            0,
+                                                            0,
+                                                            R.drawable.ic_menu);
+                } else {
+                    setCompoundDrawablesWithIntrinsicBounds(0,
+                                                            0,
+                                                            0,
+                                                            0);
+                }
+
+                setMaxLines(MAX_LINES);
+            }
+        });
+    }
+
+    @Override
+    public void setMaxLines(int maxLines) {
+        currentMaxLines = maxLines;
+        super.setMaxLines(maxLines);
+    }
+
+    /* Custom method because standard getMaxLines() requires API > 16 */
+    public int getMyMaxLines() {
+        return currentMaxLines;
+    }
+
+    @Override
+    public void onClick(View v) {
+        /* Toggle between expanded collapsed states */
+        if (getMyMaxLines() == Integer.MAX_VALUE) {
+            setMaxLines(MAX_LINES);
+        } else {
+            setMaxLines(Integer.MAX_VALUE);
+        }
+    }
+}
