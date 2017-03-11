fef6177c07810905eccbe503da6683ad84baf315
thaihn <thaik59uet@gmail.com>
2017-03-11 10:57:34 +0700

    no change

--- SchedulePatient/app/src/main/java/cupcakehakathon/com/uet/cupcake/hackathon/schedulepatient/MainActivity.java
+++ SchedulePatient/app/src/main/java/cupcakehakathon/com/uet/cupcake/hackathon/schedulepatient/MainActivity.java
@@ -189,27 +189,9 @@ public class MainActivity extends BaseActivity
                     // update list
                     SQLController controller = new SQLController(MainActivity.this);
                     ArrayList<HospitalObject> ls = controller.queryListHospital(SQLHelper.SQL_SELECT_ALL_HOSPITAL);
-<<<<<<< HEAD
                     adapter.setLsHospital(ls);
-=======
 
-                    adapter = new HospitalAdapter(ls, MainActivity.this);
->>>>>>> 677d03579352568f0fb2e58c809d2666a6e38659
-                    RecycleUtils.showListRcv(recyclerView, adapter, new Listener.listenHospital() {
-                        @Override
-                        public void onClick(int id) {
-                            Intent i = new Intent(MainActivity.this, DetailsActivity.class);
-                            i.putExtra(Constants.PASS_ID_HOSPITAL, id + 1);
-                            startActivity(i);
-                        }
-                    }, MainActivity.this);
                     break;
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 677d03579352568f0fb2e58c809d2666a6e38659
                 }
                 case PatientService.BROADCAST_ERROR_REQ_HOSPITAL: {
                     ToastUtils.quickToast(MainActivity.this, "ERROR REQUEST TO SERVER");
